3dd0b795ed39fabada761e3ce704073798d08670
Takeuchi Takaya <epianyulu2013@gmail.com>
2014-10-07 12:01:39 +0900

    DEBUGモード実装 各シーン実装

---
+++ Assets/Scripts/DebugControlScript.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugControlScript : MonoBehaviour {
+	public bool isDebugMode;
+	// FPS計測用
+	public float updateInterval = 1.0f;
+	private int   numFlame    = 0;
+	private float timeCounter = 0.0f;
+	private float timer       = 0.0f;
+	private float fps         = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		this.isDebugMode = true;
+		DontDestroyOnLoad(this);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (this.isDebugMode)
+		{
+			if (Input.GetKeyDown(KeyCode.F1)) Application.LoadLevel("DebugMenu");
+			CountFPS();
+		}
+	}
+
+	void OnGUI()
+	{
+		if (this.isDebugMode)
+		{
+			GUI.Label(new Rect(0.0f, 0.0f, 100.0f, 20.0f), "DEBUG MODE");
+			GUI.Label(new Rect(0.0f, 20.0f, 400.0f, 20.0f), "SceneName : " + Application.loadedLevelName.ToString());
+			GUI.Label(new Rect(0.0f, 40.0f, 100.0f, 20.0f), "FPS : " + this.fps.ToString());
+		}
+	}
+
+	void CountFPS()
+	{
+		this.timeCounter -= Time.deltaTime;
+		this.timer += Time.timeScale / Time.deltaTime;
+		numFlame++;
+
+		if (this.timeCounter < 0.0f)
+		{
+			this.fps = this.timer / this.numFlame;
+			this.numFlame = 0;
+			this.timer = 0;
+			this.timeCounter = this.updateInterval;
+		}
+	}
+}
---
+++ Assets/Scripts/DebugMenuScript.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugMenuScript : MonoBehaviour {
+	public GameObject debugControlScriptPrefab;
+	private GameObject debugControlScript;
+
+	// Use this for initialization
+	void Start () {
+		// デバッグ管理用スクリプトが存在しない場合は作成する
+		this.debugControlScript = GameObject.Find("DebugControlScript");
+		if (this.debugControlScript == null)
+		{
+			this.debugControlScript = Instantiate(debugControlScriptPrefab) as GameObject;
+			this.debugControlScript.name = "DebugControlScript";
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnGUI()
+	{
+		// シーン移動用
+		if (GUI.Button(new Rect(10.0f, 60.0f, 100.0f, 20.0f), "タイトル"))        { Application.LoadLevel("Title");}
+		if (GUI.Button(new Rect(10.0f, 80.0f, 100.0f, 20.0f), "待機画面"))        { Application.LoadLevel("WaitPlayer"); }
+		if (GUI.Button(new Rect(10.0f, 100.0f, 100.0f, 20.0f), "チーム選択"))     { Application.LoadLevel("ChooseTeam"); }
+		if (GUI.Button(new Rect(10.0f, 120.0f, 100.0f, 20.0f), "チュートリアル")) { Application.LoadLevel("Tutorial"); }
+		if (GUI.Button(new Rect(10.0f, 140.0f, 100.0f, 20.0f), "ゲーム"))         { Application.LoadLevel("MainGame"); }
+		if (GUI.Button(new Rect(10.0f, 160.0f, 100.0f, 20.0f), "リザルト"))       { Application.LoadLevel("Result"); }
+		if (GUI.Button(new Rect(10.0f,180.0f, 100.0f, 20.0f), "NoSetting")) { ;}
+		if (GUI.Button(new Rect(10.0f,200.0f, 100.0f, 20.0f), "NoSetting")) { ;}
+		if (GUI.Button(new Rect(10.0f,220.0f, 100.0f, 20.0f), "NoSetting")) { ;}
+		if (GUI.Button(new Rect(10.0f,240.0f, 100.0f, 20.0f), "NoSetting")) { ;}
+		if (GUI.Button(new Rect(10.0f,260.0f, 100.0f, 20.0f), "NoSetting")) { ;}
+
+		// デバッグモードチェック
+		/*
+		this.debugControlScript.AddComponent<DebugControlScript>().isDebugMode =
+			GUI.Toggle(new Rect(200.0f, 40.0f, 260.0f, 20.0f), this.debugControlScript.AddComponent<DebugControlScript>().isDebugMode, "DEBUGモード表示");
+		 */
+	}
+}
