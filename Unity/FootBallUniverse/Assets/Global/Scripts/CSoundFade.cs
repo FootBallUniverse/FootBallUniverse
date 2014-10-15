86939989109a9833e0776a09a1c9b3dbcbfd9809
KawashitaTakuya <hal.game.mail@gmail.com>
2014-10-15 17:55:24 +0900

    Globalの中身にScriptを追加。 BGMとSEをならすもので、使い方はまた教えます

---
+++ Unity/FootBallUniverse/Assets/Global/Scripts/CSoundFade.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CSoundFade : MonoBehaviour {
+
+    //----------------------------------------------------
+    // フェードのステータス
+    //----------------------------------------------------
+    enum eSOUND_STATUS 
+    {
+        eFADEIN,
+        eFADEOUT,
+        eNONE,
+    };
+
+    private eSOUND_STATUS m_status;
+
+    private const float FADE_SPEED = 0.001f;    // フェードの調整値
+
+    // フェードスピード
+    private float m_fadeSpeed;  // フェードのスピード
+    private bool m_isFade;      // フェード中かどうか
+    private AudioSource m_audioSource;
+
+
+    //----------------------------------------------------------------------
+    // Start関数より早く走る初期化関数
+    //----------------------------------------------------------------------
+    // @Param	none		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    void Awake () 
+    {
+        m_status = eSOUND_STATUS.eNONE;
+        m_fadeSpeed = FADE_SPEED;
+        m_isFade = false;
+    }
+
+    //----------------------------------------------------------------------
+    // AudioSourceのセット
+    //----------------------------------------------------------------------
+    // @Param	AudioSource SoundPlayerから送られてきたaudiosource		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void SetAudioSource(AudioSource _audioSource)
+    {
+        m_audioSource = _audioSource;
+    }
+
+    //----------------------------------------------------------------------
+    // フェードインとフェードアウトのアップデート
+    //----------------------------------------------------------------------
+    // @Param	none		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+	void Update () 
+    {
+        // フェードしているかどうか確認
+        if( m_isFade == true )
+        {
+            switch (m_status)
+            {
+                // フェードイン中
+                case eSOUND_STATUS.eFADEIN:
+                    if (m_audioSource.volume <= 1.0f)
+                    {
+                        m_audioSource.volume = m_audioSource.volume + m_fadeSpeed;
+                        if (m_audioSource.volume >= 1.0f)
+                        {
+                            // フェードイン終了
+                            m_audioSource.volume = 1.0f;
+                            m_isFade = false;
+                            m_status = eSOUND_STATUS.eNONE;
+                        }
+                    }
+                    break;
+
+
+                // フェードアウト中
+                case eSOUND_STATUS.eFADEOUT:
+                    if (m_audioSource.volume >= 0.0f)
+                    {
+                        m_audioSource.volume = m_audioSource.volume - m_fadeSpeed;
+                        if (m_audioSource.volume <= 0.0f)
+                        {
+                            // フェードアウト終了
+                            m_audioSource.volume = 0.0f;
+                            m_isFade = false;
+                            m_status = eSOUND_STATUS.eNONE;
+                        }
+                    }
+                    break;
+
+                // 何もしない
+                case eSOUND_STATUS.eNONE:
+                    break;
+            }
+        }
+	}
+
+    //----------------------------------------------------------------------
+    // フェードイン
+    //----------------------------------------------------------------------
+    // @Param	float フェードインのスピード　引数なしの場合はデフォルト		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void FadeIn(float _fadeSpeed = FADE_SPEED)
+    {
+        m_status = eSOUND_STATUS.eFADEIN;
+        m_audioSource.volume = 0.0f;        // Volumeをいったん0にする
+        m_fadeSpeed = _fadeSpeed;
+        m_isFade = true;
+    }
+
+    //----------------------------------------------------------------------
+    // フェードアウト
+    //----------------------------------------------------------------------
+    // @Param	float フェードアウトのスピード　引数なしの場合はデフォルト		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void FadeOut(float _fadeSpeed = FADE_SPEED)
+    {
+        m_status = eSOUND_STATUS.eFADEOUT;
+        m_audioSource.volume = 1.0f;        // VolumeをMaxにする
+        m_fadeSpeed = _fadeSpeed;
+        m_isFade = true;
+    }
+}
---
+++ Unity/FootBallUniverse/Assets/Global/Scripts/CSoundPlayer.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class CSoundPlayer
+{
+    public const float FADE_SPEED = 0.01f;
+
+    private GameObject m_bgmPlayerObject;  
+    private AudioSource m_bgmAudioSource;
+    private GameObject m_sePlayerObject;
+    private AudioSource m_seAudioSource;
+    private CSoundFade m_fade;
+
+
+    //----------------------------------------------------------------------
+    // コンストラクタ
+    //----------------------------------------------------------------------
+    // @Param	none		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public CSoundPlayer()
+    {
+        // BGMオブジェクトの作成
+        m_bgmPlayerObject = new GameObject("BGMObject");
+        m_bgmAudioSource = m_bgmPlayerObject.AddComponent<AudioSource>();
+
+        // フェードの設定
+        m_fade = m_bgmPlayerObject.AddComponent<CSoundFade>();
+        m_fade.SetAudioSource(m_bgmAudioSource);
+
+        // SEオブジェクトの作成
+        m_sePlayerObject = new GameObject("SEObject");
+        m_seAudioSource = m_sePlayerObject.AddComponent<AudioSource>();
+    }
+
+    //----------------------------------------------------------------------
+    // シングルトン実装
+    //----------------------------------------------------------------------
+    // @Param	none		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    private static CSoundPlayer m_soundPlayer; 
+    public static CSoundPlayer GetInstance()
+    {
+        if( m_soundPlayer == null )
+        {
+            m_soundPlayer = new CSoundPlayer();
+        }
+
+        return m_soundPlayer;
+    }
+
+    //----------------------------------------------------------------------
+    // SEの再生
+    //----------------------------------------------------------------------
+    // @Param	string 鳴らしたいSEファイルの名前		
+    // @Return	成功か失敗
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public bool PlaySE(string _audioName)
+    {
+        // 音楽データロード
+        AudioClip audioClip = (AudioClip)Resources.Load("Sound/SE/" + _audioName);
+
+        // SE再生
+        m_seAudioSource.PlayOneShot(audioClip);
+
+        return true;
+    }
+
+
+    //----------------------------------------------------------------------
+    // BGMの再生
+    //----------------------------------------------------------------------
+    // @Param	string 鳴らしたいBGMファイルの名前		
+    // @Return	成功か失敗
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public bool PlayBGM(string _audioName)
+    {
+        // 音楽データロード
+        AudioClip audioClip = (AudioClip)Resources.Load("Sound/BGM/" + _audioName);
+        m_bgmAudioSource.clip = audioClip;
+
+        // BGM再生
+        m_bgmAudioSource.loop = true;
+        m_bgmAudioSource.Play();
+
+        return true;
+    }
+
+    //----------------------------------------------------------------------
+    // 音量の調整
+    //----------------------------------------------------------------------
+    // @Param	float 調整したい量		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void ChangeVolume(float _volume)
+    {
+        m_bgmAudioSource.volume = _volume;
+    }
+
+    //----------------------------------------------------------------------
+    // 一時停止
+    //----------------------------------------------------------------------
+    // @Param	none		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void Pause()
+    {
+        m_bgmAudioSource.Pause();
+    }
+
+    //----------------------------------------------------------------------
+    // フェードイン再生
+    //----------------------------------------------------------------------
+    // @Param	string 再生したいBGMの名前 float フェードインのスピード		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void PlayBGMFadeIn(string _audioName ,float _fadeSpeed)
+    {
+        this.PlayBGM(_audioName);
+        m_fade.FadeIn(_fadeSpeed);
+    }
+
+    //----------------------------------------------------------------------
+    // フェードアウト停止
+    //----------------------------------------------------------------------
+    // @Param	float フェードアウトのスピード		
+    // @Return	none
+    // @Date	2014/10/15  @Update 2014/10/15  @Author T.Kawashita      
+    //----------------------------------------------------------------------
+    public void PlayBGMFadeOut(float _fadeSpeed)
+    {
+        m_fade.FadeOut(_fadeSpeed);
+    }
+}
