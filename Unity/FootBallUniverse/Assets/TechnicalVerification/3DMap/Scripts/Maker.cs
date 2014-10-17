a6a32be0501bcdd296a26eddd97f8546ed2dc571
Takeuchi Takaya <epianyulu2013@gmail.com>
2014-10-17 17:55:43 +0900

    竹内TEST

--- Unity/FootBallUniverse/Assets/TechnicalVerification/3DMap/Scripts/Maker.cs
+++ Unity/FootBallUniverse/Assets/TechnicalVerification/3DMap/Scripts/Maker.cs
@@ -67,14 +67,26 @@ public class Maker : MonoBehaviour {
 		workMesh.vertices = vertices;
 		workMesh.RecalculateBounds();
 
+		// Pointer
+		/*
+		workMesh = this.pointer.GetComponent<MeshFilter>().mesh;
+		vertices = workMesh.vertices;
+
+		vertices[0].x = vertices[0].y = 0.0f;
+		vertices[0].z += 5.0f;
+		vertices[3] = vertices[2] = vertices[1] = vertices[0];
+
+		workMesh.vertices = vertices;
+		workMesh.RecalculateBounds();
+		 */
+
 		// テクスチャ指定
-		this.quad.renderer.materials[0].mainTexture = Resources.Load("MapMakerTexture") as Texture;
+		//this.quad.renderer.materials[0].mainTexture = Resources.Load("MapMakerTexture") as Texture;
 		// 色設定
 		this.playerData = playerData;
+		setColor.a = 0.5f;
 		this.hightPole.renderer.material.color = setColor;
-		setColor.a = 255f;
 		this.quad.renderer.material.color = setColor;
-		setColor.a += 20.0f;
 		this.pointer.renderer.material.color = setColor;
 	}
 }
