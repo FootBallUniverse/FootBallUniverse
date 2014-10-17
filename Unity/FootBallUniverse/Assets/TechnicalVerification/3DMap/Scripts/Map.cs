41ef2e241b9da9c26018ab1f7158b4ab9f0f0127
Takeuchi Takaya <epianyulu2013@gmail.com>
2014-10-17 13:09:40 +0900

    MAP機能作成

---
+++ Unity/FootBallUniverse/Assets/TechnicalVerification/3DMap/Scripts/Map.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Map : MonoBehaviour {
+	int playerNum = 0;
+
+	public GameObject makerPrefab;
+	// Use this for initialization
+	void Start () {
+		makerPrefab = Resources.Load("MapMaker") as GameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	public void CreateMaker(GameObject playerData,Color setColor)
+	{
+		GameObject newMaker;
+		// エラーチェック
+		if (playerData == null) return;
+		// 新しいマーカー生成
+		newMaker = Instantiate(this.makerPrefab) as GameObject;
+		newMaker.transform.parent = this.transform;
+		newMaker.name = "Marker" + this.playerNum;
+		// 初期化
+		newMaker.GetComponent<Maker>().Init(playerData,setColor);
+		// 加算
+		this.playerNum++;
+	}
+}
--- Unity/FootBallUniverse/Assets/TechnicalVerification/3DMap/Scripts/Player.cs
+++ Unity/FootBallUniverse/Assets/TechnicalVerification/3DMap/Scripts/Player.cs
@@ -1,8 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class damy2 : MonoBehaviour {
-
+public class Player : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 	
