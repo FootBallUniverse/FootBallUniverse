bef4e6a68d3e2a09af72c49f2cef03dbc49d6404
Takashi Kaneko <yohhyaa@gmail.com>
2014-10-20 14:22:55 +0900

    ボール反射と３Dマップの結合

---
+++ Unity/FootBallUniverse/Assets/Test.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Test : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		GameObject.Find("Map").GetComponent<Map>().CreateMaker(GameObject.Find("Ball").gameObject,new Color(1,0,0,1));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
