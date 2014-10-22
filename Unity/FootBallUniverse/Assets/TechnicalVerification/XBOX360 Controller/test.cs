d91a0cfb4b644a3a555b332f3525cd23011ce99b
KawashitaTakuya <hal.game.mail@gmail.com>
2014-10-22 20:03:28 +0900

    Excelのテスト時に発生していたバグを修正

--- Unity/FootBallUniverse/Assets/TechnicalVerification/XBOX360 Controller/test.cs
+++ Unity/FootBallUniverse/Assets/TechnicalVerification/XBOX360 Controller/test.cs
@@ -1,13 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
-using Microsoft.Office.Interop.Excel;
 
 public class test : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
 
-        Microsoft.Office.Interop.Excel.Application excelApli;
 
     }
 	
