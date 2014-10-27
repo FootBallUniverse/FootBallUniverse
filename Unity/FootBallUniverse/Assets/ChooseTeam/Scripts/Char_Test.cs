3489a89903b4142175b1766145915599cab3ce3c
Shota Iwata <i.showtime_1226@gmail.com>
2014-10-27 13:17:05 +0900

    チーム選択画面1体回転できたやつ

---
+++ Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Char_Test.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Char_Test : MonoBehaviour {
+
+	float degree;
+	float r;
+	float centerx;
+	float centerz;
+	float radian;
+	
+	// Use this for initialization
+	void Start () {
+		degree = 0.0f;
+		r = 0.31f;
+		centerx = 0.0f;
+		centerz = 0.0f;
+		radian = 0.0f;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 Position = transform.position;
+		radian = Mathf.PI/180.0f*degree;
+		Position.x = centerx+r*Mathf.Cos(radian);
+		Position.z = centerz+r*Mathf.Sin(radian)/2;
+		//this._xscale = this._yscale=this._alpha=this._y;
+		degree += 5.0f;
+		Debug.Log(degree);
+		transform.position = Position;
+		if(degree >= 360.0f){
+			degree =0.0f;
+		}
+	
+	}
+}
---
+++ Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Player_Rotate.cs
@@ -0,0 +1,65 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Rotate : MonoBehaviour {
+
+	int m_Flag,m_Count;
+	// 速度
+	public Vector2 SPEED = new Vector2(0.05f, 0.01f);
+	
+	// Use this for initialization
+	void Start () {
+		m_Flag = 0;
+		m_Count = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		switch(m_Flag){
+			case 0:
+				transform.position -= transform.right * 0.01f;
+				transform.position -= transform.forward * 0.01f;
+				m_Count ++;
+				
+				if(m_Count >= 31){
+					m_Flag = 1;
+					m_Count = 0;
+				}
+					
+				break;
+			case 1:
+				transform.position -= transform.right * 0.01f;
+				transform.position += transform.forward * 0.01f;
+				m_Count ++;
+				
+				if(m_Count >= 31){
+					m_Flag = 2;
+					m_Count = 0;
+				}
+					
+				break;
+			case 2:
+				transform.position += transform.right * 0.01f;
+				transform.position += transform.forward * 0.01f;
+				m_Count ++;
+				
+				if(m_Count >= 31){
+					m_Flag = 3;
+					m_Count = 0;
+				}
+					
+				break;
+			case 3:
+				transform.position += transform.right * 0.01f;
+				transform.position -= transform.forward * 0.01f;
+				m_Count ++;
+				
+				if(m_Count >= 31){
+					m_Flag = 0;
+					m_Count = 0;
+				}
+					
+				break;
+		}
+	}
+}
