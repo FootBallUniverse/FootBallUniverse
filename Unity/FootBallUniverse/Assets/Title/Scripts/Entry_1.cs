cf6abb687edf20640f1155ee56d156fe3065b827
Shota Iwata <i.showtime_1226@gmail.com>
2014-10-31 20:26:57 +0900

    タイトル画面更新

--- Unity/FootBallUniverse/Assets/Title/Scripts/Entry_1.cs
+++ Unity/FootBallUniverse/Assets/Title/Scripts/Entry_1.cs
@@ -1,56 +1,60 @@
 ﻿using UnityEngine;
 using System.Collections;
- 
-public class Entry_1 : MonoBehaviour {
 
-	// 速度
-	public Vector2 SPEED = new Vector2(0.05f, 0.01f);
-
+public class Entry_1 : MonoBehaviour
+{
+    // 速度
+    public Vector2 SPEED = new Vector2(0.05f, 0.01f);
     // エントリーしたかどうかの確認用フラグ
-	private bool m_inFlag;
-	public static int m_upCount;
-	
-	// Use this for initialization
-	void Start () {
+    public bool m_inFlag = false;
+    // 初期位置保存用
+    Vector3 Position;  
+    
+    // Use this for initialization
+    void Start()
+    {
+        // キャンセル用に初期位置を保存
+        Position = new Vector3(transform.position.x, transform.position.y, transform.position.z);
         m_inFlag = false;
-        m_upCount = 0;
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		// 移動処理
-		Move();
-	}
-	
-	// 移動関数
-	void Move(){
-	
-		// 現在位置をPositionに代入
-		Vector2 Position = transform.position;
-		
-        // エントリーできるかどうか
-		if(m_inFlag == false)
-		{
-			// エントリー
-			if(Input.GetKeyDown(KeyCode.Alpha1) ||
-               Input.GetKeyDown(InputXBOX360.P1_XBOX_A) )
-			{
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 移動処理
+        Move();
+    }
+
+    // 移動関数
+    void Move()
+    {
+        // エントリー出来るかどうか
+        if (m_inFlag == false)
+        {
+            // エントリー
+            if (Input.GetKeyDown(KeyCode.Alpha1) ||
+                Input.GetKeyDown(KeyCode.Space) ||
+                Input.GetKeyDown(InputXBOX360.P1_XBOX_A))
+            {
                 Debug.Log("Player1 Entry");
-				m_inFlag = true;
-			}
-		}
+                m_inFlag = true;
+                // 代入したPositionに対して大きな値を代入し、テクスチャを画面外へ吹っ飛ばす
+                transform.position = new Vector3(transform.position.x, 2048.0f, transform.position.z);
+            }
+        }
 
-		if(m_inFlag == true && m_upCount <=10)
-		{
-			// 代入したPositionに対して加算減算を行う
-			Position.y += SPEED.y;
-			// フラグをTrueに変えて再度加算しないようにする
-			m_upCount ++;
-		}
-
-		// 現在の位置に加算減算を行ったPositionを代入する
-		transform.position = Position;
-	}
- 
- 
+        // エントリーキャンセルする場合
+        if (m_inFlag == true)
+        {
+            // エントリーキャンセル
+            if (Input.GetKeyDown(KeyCode.Q) ||
+                Input.GetKeyDown(InputXBOX360.P1_XBOX_B))
+            {
+                Debug.Log("Player1 FAILED");
+                m_inFlag = false;
+                // 代入したPositionに対して大きな値を代入し、テクスチャを画面外へ吹っ飛ばす
+                transform.position = Position;
+            }
+        }
+    }
 }
--- Unity/FootBallUniverse/Assets/Title/Scripts/title.cs
+++ Unity/FootBallUniverse/Assets/Title/Scripts/title.cs
@@ -2,22 +2,50 @@
 using System.Collections;
 
 public class title : MonoBehaviour {
+    
+    private int m_StartCount;
+
+    public GameObject entry1;
+    public GameObject entry2;
+    public GameObject entry3;
+    public GameObject entry4;
 
 	// Use this for initialization
-	void Start () {
-	
-	}
-	
+    void Start()
+    {
+        m_StartCount = 0;
+
+        entry1 = this.transform.FindChild("entry_wait_state_1").gameObject;
+        entry2 = this.transform.FindChild("entry_wait_state_2").gameObject;
+        entry3 = this.transform.FindChild("entry_wait_state_3").gameObject;
+        entry4 = this.transform.FindChild("entry_wait_state_4").gameObject;
+    
+    }
+
 	// Update is called once per frame
 	void Update () 
     {
-		if(Entry_1.m_upCount >= 10 && Entry_2.m_upCount >= 10 && Entry_3.m_upCount >= 10 && Entry_4.m_upCount >= 10 )
-		{
-            // ここにアニメーションの開始を入れる
+       Entry_1 entry_1 = entry1.GetComponent<Entry_1>();
+       Entry_2 entry_2 = entry2.GetComponent<Entry_2>();
+       Entry_3 entry_3 = entry3.GetComponent<Entry_3>();
+       Entry_4 entry_4 = entry4.GetComponent<Entry_4>();
 
-			Application.LoadLevel("ChooseTeam");
-		}
-
+        if (entry_1.m_inFlag == true &&
+            entry_2.m_inFlag == true &&
+            entry_3.m_inFlag == true &&
+            entry_4.m_inFlag == true)
+        {
+            if (m_StartCount >= 30)
+            {
+                Application.LoadLevel("ChooseTeam");
+            }
+            m_StartCount++;
+        }
+        else
+        {
+            m_StartCount = 0;
+        }
+       
         // スペースキーで強制的に次のシーンに飛ばす
 		if(Input.GetKeyDown(KeyCode.Space))
 		{
