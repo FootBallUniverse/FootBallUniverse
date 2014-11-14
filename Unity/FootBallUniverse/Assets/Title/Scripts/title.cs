6d558c3986200275a3e7c2512015a0d03bdb47ea
Shota Iwata <i.showtime_1226@gmail.com>
2014-11-14 13:13:12 +0900

    タイトル修正

--- Unity/FootBallUniverse/Assets/Title/Scripts/title.cs
+++ Unity/FootBallUniverse/Assets/Title/Scripts/title.cs
@@ -2,13 +2,15 @@
 using System.Collections;
 
 public class title : MonoBehaviour {
-    
-    public int m_StartCount;
 
-    public Entry_1 m_entry1;
-    public Entry_2 m_entry2;
-    public Entry_3 m_entry3;
-    public Entry_4 m_entry4;
+    private int m_StartCount;
+
+    private Entry_1     m_entry1;
+    private Entry_2     m_entry2;
+    private Entry_3     m_entry3;
+    private Entry_4     m_entry4;
+    private Title_Fade  m_Fade;
+    public  bool        m_SceneFlag;
 	// Use this for initialization
     void Start()
     {
@@ -18,11 +20,12 @@ public class title : MonoBehaviour {
         GameObject entry2 = this.transform.FindChild("entry_wait_state_2").gameObject;
         GameObject entry3 = this.transform.FindChild("entry_wait_state_3").gameObject;
         GameObject entry4 = this.transform.FindChild("entry_wait_state_4").gameObject;
-    
+        GameObject Fade   = this.transform.FindChild("Title_Fade").gameObject;
         m_entry1 = entry1.GetComponent<Entry_1>();
         m_entry2 = entry2.GetComponent<Entry_2>();
         m_entry3 = entry3.GetComponent<Entry_3>();
         m_entry4 = entry4.GetComponent<Entry_4>();
+        m_Fade   = Fade.GetComponent<Title_Fade>();
 
 
     }
@@ -36,14 +39,27 @@ public class title : MonoBehaviour {
             m_entry3.m_inFlag == true &&
             m_entry4.m_inFlag == true)
         {
-            if (m_StartCount >= 30)
+            m_SceneFlag = true;
+        }
+        else
+        {
+            m_SceneFlag = false;
+        }
+        if (m_SceneFlag == true)
+        {
+            if (m_StartCount >= 100)
             {
                 Application.LoadLevel("ChooseTeam");
+            }
+            else if (m_StartCount >= 20)
+            {
+                m_Fade.m_FadeFlag = true;
             }
             m_StartCount++;
         }
         else
         {
+            m_Fade.m_FadeFlag = false;
             m_StartCount = 0;
         }
        
