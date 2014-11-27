023cd8c8d3ec4fd4e2a8c7e9643068350f2c7ca0
Takashi Kaneko <yohhyaa@gmail.com>
2014-11-27 15:30:30 +0900

    コリジョンファイルの上げ忘れ

---
+++ Unity/FootBallUniverse/Assets/TechnicalVerification/Collision/CPlayerCollision.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CPlayerCollision : MonoBehaviour {
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if(other.gameObject.tag=="SoccerBall")
+		{
+			if(other.transform.parent == GameObject.Find("BallGameObject").transform)
+			{
+				// ボールの位置をセット
+				Vector3 pos = new Vector3(0.0f,0.05f,0.1f);
+				
+				// プレイヤーのボールに設定
+				CPlayerManager.m_playerManager.m_soccerBallManager.ChangeOwner(this.transform,pos);
+
+				this.gameObject.GetComponent<CPlayer>().m_isBall = true;
+
+			}
+		}
+
+	}
+
+}
