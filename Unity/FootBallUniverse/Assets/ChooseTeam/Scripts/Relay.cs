792750f27ca5a13524215cae32c216b269e12b63
Shota Iwata <i.showtime_1226@gmail.com>
2014-11-28 14:19:33 +0900

    タイトル、チーム選択の画面サイズ修正

--- Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Relay.cs
+++ Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Relay.cs
@@ -16,8 +16,7 @@ public class Relay : MonoBehaviour {
     private Vector3[] m_Vec1 = new Vector3[4];
     private Vector3[] m_Vec2 = new Vector3[4];
     private Vector3[] m_Relay = new Vector3[2];
-    private GameObject m_Relay1;
-    private GameObject m_Relay2;
+    private UILabel m_Label;
 
 
 	// Use this for initialization
@@ -32,9 +31,6 @@ public class Relay : MonoBehaviour {
         GameObject m_ENG2 = this.gameObject.transform.FindChild("England_2").gameObject;
         GameObject m_BR2  = this.gameObject.transform.FindChild("Brazil_2").gameObject;
 
-        m_Relay1 = this.gameObject.transform.FindChild("Relay_1").gameObject;
-        m_Relay2 = this.gameObject.transform.FindChild("Relay_2").gameObject;
-
         GameObject m_Player1 = GameObject.Find("Team1_2");
         GameObject m_Player3 = GameObject.Find("Team3_4");
         m_Team1 = m_Player1.transform.GetComponent<Player_1_Script>();
@@ -47,7 +43,7 @@ public class Relay : MonoBehaviour {
         m_PosSP2  = m_SP2.GetComponent<Relay_SP2>(); 
         m_PosENG2 = m_ENG2.GetComponent<Relay_ENG2>();
         m_PosBR2  = m_BR2.GetComponent<Relay_BR2>();
-
+        m_Label = GameObject.Find("Label(Relay)").GetComponent<UILabel>();
         m_Vec1[3] = m_PosJP1.transform.position;
         m_Vec1[0] = m_PosSP1.transform.position;
         m_Vec1[1] = m_PosENG1.transform.position;
@@ -56,10 +52,7 @@ public class Relay : MonoBehaviour {
         m_Vec2[0] = m_PosSP2.transform.position;
         m_Vec2[1] = m_PosENG2.transform.position;
         m_Vec2[2] = m_PosBR2.transform.position;
-        m_Relay[0] = m_Relay1.transform.position;
-        m_Relay[1] = m_Relay2.transform.position;
-        m_Relay[0].y = 2.0f;
-        m_Relay[1].y = 2.0f;
+        
 	}
 
 	// Update is called once per frame
@@ -87,19 +80,28 @@ public class Relay : MonoBehaviour {
             // 
             if (m_Team1.m_Fade_flag_1.m_FadeFlag == 2)
             {
-                m_Relay[0].y = 0.18f;
+                //m_Relay[0].y = 0.18f;
             }
             else
             {
-                m_Relay[0].y = 2.0f;
+               // m_Relay[0].y = 2.0f;
             }
             if (m_Team3.m_Fade_flag_2.m_FadeFlag == 2)
             {
-                m_Relay[1].y = 0.18f;
+               // m_Relay[1].y = 0.18f;
             }
             else
             {
-                m_Relay[1].y = 2.0f;
+               // m_Relay[1].y = 2.0f;
+            }
+            if (m_Team1.m_Fade_flag_1.m_FadeFlag == 2
+                && m_Team3.m_Fade_flag_2.m_FadeFlag == 2)
+            {
+                m_Label.text = "ゲームを開始します。";
+            }
+            else
+            {
+                m_Label.text = "チームを選択中です・・・";
             }
         }
 
@@ -111,8 +113,6 @@ public class Relay : MonoBehaviour {
         m_PosSP2.transform.position  = m_Vec2[0];
         m_PosENG2.transform.position = m_Vec2[1];
         m_PosBR2.transform.position  = m_Vec2[2];
-        m_Relay1.transform.position = m_Relay[0];
-        m_Relay2.transform.position = m_Relay[1];
 	}
     
 }            
