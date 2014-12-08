c2f1f637b76720be28473f98b939ba5777a11eb6
KawashitaTakuya <hal.game.mail@gmail.com>
2014-12-08 20:03:06 +0900

    音変更　ゴールの処理変更（バグ有）

---
+++ Unity/FootBallUniverse/Assets/Game/Scripts/Stage/C1P2PGoalStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class C1P2PGoalStart : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.transform.tag == "Player")
+        {
+             this.transform.parent.FindChild("goal1_collision").GetComponent<BoxCollider>().isTrigger = false;
+        }
+        else if (collider.transform.tag == "SoccerBall")
+        {
+            this.transform.parent.FindChild("goal1_collision").GetComponent<BoxCollider>().isTrigger = true;
+        }
+    }
+}
