c362ad0a563fbeb6b572c18a06574d9a487fd984
Takeuchi Takaya <epianyulu2013@gmail.com>
2014-12-08 14:49:55 +0900

    リザルト画面に新仕様追加＋一部画像差し替え＋スクリプト改変＋テクスチャ追加

--- Unity/FootBallUniverse/Assets/Result/Scripts/Result.cs
+++ Unity/FootBallUniverse/Assets/Result/Scripts/Result.cs
@@ -12,14 +12,18 @@ public class Result : MonoBehaviour {
 		MOVING_TEAM_SUPPORTER,
 		ALPHA_WORLD_SUPPORTER,
 		ADDING_WORLD_SUPPORTER,
+		STAY_TWO,
+		ALPHA_THANK_YOU,
 		STAY_LAST,
 		ALPHA_OUT,
+		RESULT_END,
 		STATE_MAX
 	};
 
-	private RESULT_STATE state     = RESULT_STATE.ALPHA_IN;
-	private GameObject[] SubPanels = new GameObject[4];
-	private int[] works            = new int[2];
+	private RESULT_STATE[] state    = new RESULT_STATE[2];
+	private GameObject[] SubPanels  = new GameObject[6];
+	private int[] works             = new int[2];
+	private bool[,] buttonCheck     = new bool[2,2];
 	public  int AddSuppoterTime;
 
     public CSoundPlayer m_soundPlayer;
@@ -38,6 +42,8 @@ public class Result : MonoBehaviour {
         Init();
         m_soundPlayer = new CSoundPlayer();
 
+		for (int i = 0; i < 2; i++) this.state[i] = RESULT_STATE.ALPHA_IN;
+		for (int i = 0; i < 2; i++) for(int j = 0; j < 2; j ++) this.buttonCheck[i,j] = false;
         m_soundPlayer.PlayBGMFadeIn("result/bgm_01", 0.002f);
     }
 
@@ -52,42 +58,41 @@ public class Result : MonoBehaviour {
 	{
 		GameObject[] panels = new GameObject[3];
 
-		// ドライバ
-#if false
+#if true // ドライバ
 		TeamData.teamNationality[0] = TeamData.TEAM_NATIONALITY.JAPAN;
 		TeamData.teamNationality[1] = TeamData.TEAM_NATIONALITY.ESPANA;
 
-		TeamData.AddLog(0, 1, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(1, 2, 0, true);
-		TeamData.AddLog(2, 3, 1, true);
-		TeamData.AddLog(3, 4, 1, true);
-		TeamData.AddLog(4, 4, 1, false);
+		TeamData.AddLog(0, 1, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(1, 2, 0, 1, true);
+		TeamData.AddLog(2, 3, 1, 0, true);
+		TeamData.AddLog(3, 4, 1, 0, true);
+		TeamData.AddLog(4, 4, 1, 0, false);
 
 		TeamData.suppoterByTeam[0] = 1234;
 		TeamData.suppoterByTeam[1] = 5233;
 		TeamData.suppoterByWorld = 100000000;
-#endif
-		// ドライバ＿END
+#endif // ドライバ＿END
 
 		// パネルデータ読込
-		panels[0]    = GameObject.Find("MainPanel") as GameObject;
-		panels[1]    = GameObject.Find("SubPanel0") as GameObject;
-		panels[2]    = GameObject.Find("SubPanel1") as GameObject;
+		panels[0]    = GameObject.Find("MainPanel")  as GameObject;
+		panels[1]    = GameObject.Find("SubPanel0")  as GameObject;
+		panels[2]    = GameObject.Find("SubPanel1")  as GameObject;
 		SubPanels[0] = GameObject.Find("SubPanel00") as GameObject;
 		SubPanels[1] = GameObject.Find("SubPanel01") as GameObject;
 		SubPanels[2] = GameObject.Find("SubPanel10") as GameObject;
 		SubPanels[3] = GameObject.Find("SubPanel11") as GameObject;
-
+		SubPanels[4] = GameObject.Find("SubPanel02") as GameObject;
+		SubPanels[5] = GameObject.Find("SubPanel12") as GameObject;
 
 		for (int j = 0; j < 3; j++)
 		{
@@ -103,19 +108,19 @@ public class Result : MonoBehaviour {
 				{
 					case TeamData.TEAM_NATIONALITY.BRASIL:
 						panels[j].transform.FindChild("Flag" + i).GetComponent<UISprite>().spriteName = "BRA_type1";
-						panels[j].transform.FindChild("CountryName" + i).GetComponent<UILabel>().text = "ブラジル";
+						panels[j].transform.FindChild("CountryName" + i).GetComponent<UISprite>().spriteName = "name_Brazil_"+i;
 						break;
 					case TeamData.TEAM_NATIONALITY.ENGLAND:
 						panels[j].transform.FindChild("Flag" + i).GetComponent<UISprite>().spriteName = "ENG_type1";
-						panels[j].transform.FindChild("CountryName" + i).GetComponent<UILabel>().text = "イングランド";
+						panels[j].transform.FindChild("CountryName" + i).GetComponent<UISprite>().spriteName = "name_England_" + i;
 						break;
 					case TeamData.TEAM_NATIONALITY.ESPANA:
 						panels[j].transform.FindChild("Flag" + i).GetComponent<UISprite>().spriteName = "ESP_type1";
-						panels[j].transform.FindChild("CountryName" + i).GetComponent<UILabel>().text = "スペイン";
+						panels[j].transform.FindChild("CountryName" + i).GetComponent<UISprite>().spriteName = "name_Spain_" + i;
 						break;
 					case TeamData.TEAM_NATIONALITY.JAPAN:
 						panels[j].transform.FindChild("Flag" + i).GetComponent<UISprite>().spriteName = "JPN_type1";
-						panels[j].transform.FindChild("CountryName" + i).GetComponent<UILabel>().text = "日本";
+						panels[j].transform.FindChild("CountryName" + i).GetComponent<UISprite>().spriteName = "name_Japan_" + i;
 						break;
 				}
 			}
@@ -158,104 +163,150 @@ public class Result : MonoBehaviour {
 		this.SubPanels[2].transform.FindChild("NUM_Supporter_World").GetComponent<DrawNumber>().number = TeamData.suppoterByWorld;
 		this.SubPanels[3].transform.FindChild("NUM_Supporter_Team0").GetComponent<DrawNumber>().number = TeamData.suppoterByTeam[0];
 		this.SubPanels[3].transform.FindChild("NUM_Supporter_Team1").GetComponent<DrawNumber>().number = TeamData.suppoterByTeam[1];
+		Debug.Log("LEFT = " + this.state[0] + "  RIGHT = " + this.state[1]);
 
-		switch (this.state)
+		// ボタンチェック
+		if(Input.GetKeyDown(InputXBOX360.P1_XBOX_A)) this.buttonCheck[0,0] = true;
+		if(Input.GetKeyDown(InputXBOX360.P2_XBOX_A)) this.buttonCheck[0,1] = true;
+		if(Input.GetKeyDown(InputXBOX360.P3_XBOX_A)) this.buttonCheck[1,0] = true;
+		if(Input.GetKeyDown(InputXBOX360.P4_XBOX_A)) this.buttonCheck[1,1] = true;
+		if(Input.GetKeyDown(KeyCode.LeftShift)) this.buttonCheck[0,0]  = this.buttonCheck[0,1] = true;
+		if(Input.GetKeyDown(KeyCode.RightShift)) this.buttonCheck[1,0] = this.buttonCheck[1,1] = true;
+
+		// 遷移
+		for (int i = 0; i < 2; i++)
 		{
-			case RESULT_STATE.ALPHA_IN:
-				if (GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().enabled == false)
-				{
-					this.state = RESULT_STATE.STAY_FIRST;
-				}
-				break;
-			case RESULT_STATE.STAY_FIRST:
-				if (Input.GetKeyDown(KeyCode.Space))
-				{
-					this.SubPanels[1].GetComponent<TweenAlpha>().enabled = true;
-					this.SubPanels[3].GetComponent<TweenAlpha>().enabled = true;
-					this.state = RESULT_STATE.ALPHA_TEAM_SUPPORTER;
-				}
-				break;
+			switch (this.state[i])
+			{
+				// 最初のフェードイン
+				case RESULT_STATE.ALPHA_IN:
+					ReSetButtonCheck();
+					if (GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().enabled == false)
+						this.state[i] = RESULT_STATE.STAY_FIRST;
+					break;
 
-			case RESULT_STATE.ALPHA_TEAM_SUPPORTER:
-				if (this.SubPanels[1].GetComponent<TweenAlpha>().enabled == false)
-				{
-					this.state = RESULT_STATE.STAY_TEAM_SUPPORTER;
-				}
-				break;
-
-			case RESULT_STATE.STAY_TEAM_SUPPORTER:
-				if (Input.GetKeyDown(KeyCode.Space))
-				{
-					this.SubPanels[1].GetComponent<TweenPosition>().enabled = true;
-					this.SubPanels[3].GetComponent<TweenPosition>().enabled = true;
-					this.state = RESULT_STATE.MOVING_TEAM_SUPPORTER;
-				}
-				break;
-
-			case RESULT_STATE.MOVING_TEAM_SUPPORTER:
-				if (this.SubPanels[1].GetComponent<TweenPosition>().enabled == false)
-				{
-					this.SubPanels[0].GetComponent<TweenAlpha>().enabled = true;
-					this.SubPanels[2].GetComponent<TweenAlpha>().enabled = true;
-					this.state = RESULT_STATE.ALPHA_WORLD_SUPPORTER;
-				}
-				break;
-
-			case RESULT_STATE.ALPHA_WORLD_SUPPORTER:
-				if (this.SubPanels[0].GetComponent<TweenAlpha>().enabled == false)
-				{
-					this.state = RESULT_STATE.ADDING_WORLD_SUPPORTER;
-					this.SubPanels[0].GetComponent<TweenScale>().Play(true);
-					this.SubPanels[2].GetComponent<TweenScale>().Play(true);
-					works[0] = TeamData.suppoterByTeam[0] / this.AddSuppoterTime;
-					works[1] = TeamData.suppoterByTeam[1] / this.AddSuppoterTime;
-				}
-				break;
-
-			case RESULT_STATE.ADDING_WORLD_SUPPORTER:
-				if (TeamData.suppoterByTeam[0] == 0 && TeamData.suppoterByTeam[1] == 0)
-				{
-					this.state = RESULT_STATE.STAY_LAST;
-					this.SubPanels[0].GetComponent<TweenScale>().enabled = false;
-					this.SubPanels[2].GetComponent<TweenScale>().enabled = false;
-				}
-				else
-				{
-					for (int i = 0; i < 2; i++)
+				// 待機その１
+				case RESULT_STATE.STAY_FIRST:
+					
+					if (this.buttonCheck[i,0] == true && this.buttonCheck[i,1] == true)
 					{
-						if (TeamData.suppoterByTeam[i] >= works[i])
+						this.SubPanels[1].GetComponent<TweenAlpha>().enabled = true;
+						this.SubPanels[3].GetComponent<TweenAlpha>().enabled = true;
+						this.state[i] = RESULT_STATE.ALPHA_TEAM_SUPPORTER;
+					}
+					break;
+
+				// チームサポーターがフェードイン
+				case RESULT_STATE.ALPHA_TEAM_SUPPORTER:
+					ReSetButtonCheck();
+					if (this.SubPanels[1].GetComponent<TweenAlpha>().enabled == false)
+					{
+						this.state[i] = RESULT_STATE.STAY_TEAM_SUPPORTER;
+					}
+					break;
+
+				// チームサポーターが表示状態で待機
+				case RESULT_STATE.STAY_TEAM_SUPPORTER:
+					if (this.buttonCheck[i,0] == true && this.buttonCheck[i,1] == true)
+					{
+						this.SubPanels[1].GetComponent<TweenPosition>().enabled = true;
+						this.SubPanels[3].GetComponent<TweenPosition>().enabled = true;
+						this.state[i] = RESULT_STATE.MOVING_TEAM_SUPPORTER;
+					}
+					break;
+
+				// チームサポーターが上へ移動
+				case RESULT_STATE.MOVING_TEAM_SUPPORTER:
+					ReSetButtonCheck();
+					if (this.SubPanels[1].GetComponent<TweenPosition>().enabled == false)
+					{
+						this.SubPanels[0].GetComponent<TweenAlpha>().enabled = true;
+						this.SubPanels[2].GetComponent<TweenAlpha>().enabled = true;
+						this.state[i] = RESULT_STATE.ALPHA_WORLD_SUPPORTER;
+					}
+					break;
+
+				// 全サポーターがフェードイン
+				case RESULT_STATE.ALPHA_WORLD_SUPPORTER:
+					ReSetButtonCheck();
+					if (this.SubPanels[0].GetComponent<TweenAlpha>().enabled == false)
+					{
+						this.state[i] = RESULT_STATE.ADDING_WORLD_SUPPORTER;
+						this.SubPanels[0].GetComponent<TweenScale>().Play(true);
+						this.SubPanels[2].GetComponent<TweenScale>().Play(true);
+						works[0] = TeamData.suppoterByTeam[0] / this.AddSuppoterTime;
+						works[1] = TeamData.suppoterByTeam[1] / this.AddSuppoterTime;
+					}
+					break;
+
+				// 全サポーターに加算
+				case RESULT_STATE.ADDING_WORLD_SUPPORTER:
+					ReSetButtonCheck();
+					if (TeamData.suppoterByTeam[0] == 0 && TeamData.suppoterByTeam[1] == 0)
+					{
+						this.state[i] = RESULT_STATE.STAY_TWO;
+						this.SubPanels[0].GetComponent<TweenScale>().enabled = false;
+						this.SubPanels[2].GetComponent<TweenScale>().enabled = false;
+					}
+					else
+					{
+						for (int j = 0; j < 2; j++)
 						{
-							TeamData.suppoterByTeam[i] -= works[i];
-							TeamData.suppoterByWorld   += works[i];
-						}
-						else if (TeamData.suppoterByTeam[i] != 0)
-						{
-							TeamData.suppoterByTeam[i] --;
-							TeamData.suppoterByWorld   ++;
+							if (TeamData.suppoterByTeam[j] >= works[j])
+							{
+								TeamData.suppoterByTeam[j] -= works[j];
+								TeamData.suppoterByWorld += works[j];
+							}
+							else if (TeamData.suppoterByTeam[j] != 0)
+							{
+								TeamData.suppoterByTeam[j]--;
+								TeamData.suppoterByWorld++;
+							}
 						}
 					}
-				}
-				break;
+					break;
 
-			case RESULT_STATE.STAY_LAST:
-				if (Input.GetKeyDown(KeyCode.Space))
-				{
-					GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().Play(false);
-                    m_soundPlayer.PlayBGMFadeOut(0.004f);
-					this.state = RESULT_STATE.ALPHA_OUT;
-				}
-				break;
+				// 全サポーターが加算終了し、待機中
+				case RESULT_STATE.STAY_TWO:
+					if (this.buttonCheck[i,0] == true && this.buttonCheck[i,1] == true)
+					{
+						this.state[i] = RESULT_STATE.ALPHA_THANK_YOU;
+						this.SubPanels[4].GetComponent<TweenAlpha>().enabled = true;
+						this.SubPanels[5].GetComponent<TweenAlpha>().enabled = true;
+					}
+					break;
 
-			case RESULT_STATE.ALPHA_OUT:
-				if (GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().enabled == false)
-				{
-					Application.LoadLevel("Title");
-				}
-				break;
+				// Thank you for Playing!! がフェードイン
+				case RESULT_STATE.ALPHA_THANK_YOU:
+					if (this.SubPanels[4].GetComponent<TweenAlpha>().enabled == false)
+						this.state[i] = RESULT_STATE.STAY_LAST;
+					break;
+
+				// 最終待機
+				case RESULT_STATE.STAY_LAST:
+					if (this.buttonCheck[i,0] == true && this.buttonCheck[i,1] == true)
+					{
+						GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().Play(false);
+						m_soundPlayer.PlayBGMFadeOut(0.004f);
+						this.state[i] = RESULT_STATE.ALPHA_OUT;
+					}
+					break;
+
+				// フェードアウト
+				case RESULT_STATE.ALPHA_OUT:
+					if (GameObject.Find("FeedPanel").GetComponent<TweenAlpha>().enabled == false)
+						this.state[i] = RESULT_STATE.RESULT_END;
+					break;
+
+				// もう片方終了待ち
+				case RESULT_STATE.RESULT_END:
+					if(this.state[0] == this.state[1])
+						Application.LoadLevel("Title");
+					break;
+			}
 		}
 
-#if false
-		// デバッグ用スペースキーが押されたら強制的にタイトル画面へ
+#if false // デバッグ用スペースキーが押されたら強制的にタイトル画面へ
 		if (Input.GetKeyDown(KeyCode.Space) ||
 			InputXBOX360.IsGetAllStartButton() )
 		{ 
@@ -265,6 +316,19 @@ public class Result : MonoBehaviour {
 			Debug.Log("Title画面に遷移");
 		}
 #endif
+	}
+
+
+	//----------------------------------------------------------------------
+	// ボタンチェックを初期化
+	//----------------------------------------------------------------------
+	// @Param   none
+	// @Return  none
+	// @Date    2014/12/08  @Update 2014/12/08  @Author T.Takeuichi
+	//----------------------------------------------------------------------
+	void ReSetButtonCheck()
+	{
+		for(int i = 0; i < 2; i++) for(int j = 0; j < 2; j++) this.buttonCheck[i,j] = false;
 	}
 }
 
