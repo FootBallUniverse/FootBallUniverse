91f7f0c4b1e17fd3ade91d3580bcec10ec3e7328
Shota Iwata <i.showtime_1226@gmail.com>
2014-12-18 17:31:41 +0900

    チーム選択画面の国旗の修正

--- Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Flag_Select1.cs
+++ Unity/FootBallUniverse/Assets/ChooseTeam/Scripts/Flag_Select1.cs
@@ -5,10 +5,10 @@ public class Flag_Select1 : MonoBehaviour
 {
     public Player_1_Script m_team1;
     public GameObject[] m_Country = new GameObject[4];
-    public GameObject m_Cursor;
     public Vector3[] m_FlagPos = new Vector3[4];
     public Vector3[] m_MinScale = new Vector3[4];
     public Vector3[] m_MaxScale = new Vector3[4];
+    public TweenAlpha[] m_Alpha = new TweenAlpha[4];
     public Vector3 m_PlusScale;
     int[] m_Flag = new int[4];
     // Use this for initialization
@@ -16,9 +16,13 @@ public class Flag_Select1 : MonoBehaviour
     {
         GameObject m_Team_UI = this.gameObject.transform.parent.gameObject;
         m_Country[0] = this.gameObject.transform.FindChild("flag_SPA1").gameObject;
+        m_Alpha[0] = m_Country[0].GetComponent<TweenAlpha>();
         m_Country[1] = this.gameObject.transform.FindChild("flag_ENG1").gameObject;
+        m_Alpha[1] = m_Country[1].GetComponent<TweenAlpha>();
         m_Country[2] = this.gameObject.transform.FindChild("flag_BRA1").gameObject;
+        m_Alpha[2] = m_Country[2].GetComponent<TweenAlpha>();
         m_Country[3] = this.gameObject.transform.FindChild("flag_JAP1").gameObject;
+        m_Alpha[3] = m_Country[3].GetComponent<TweenAlpha>();
 
         m_team1 = m_Team_UI.GetComponent<Player_1_Script>();
 
@@ -35,22 +39,60 @@ public class Flag_Select1 : MonoBehaviour
     {
         for (int i = 0; i < 4; i++)
         {
+            m_Flag[i] = m_team1.m_Country[i].m_Flag;
             if (m_team1.m_Left_RotateFlag == true)
             {
-                m_FlagPos[i].x -= 0.01f;
-
-                if (m_FlagPos[i].x <= -0.3f)
+                switch (m_Flag[i])
                 {
-                    m_FlagPos[i].x = 0.49f;
+                    case 0:
+                        m_FlagPos[i].x -= 0.01f;
+                        break;
+                    case 1:
+                        m_FlagPos[i].x -= 0.01f;
+                        break;
+                    case 2:
+                        m_FlagPos[i].x -= 0.042f;
+                        if (m_FlagPos[i].x <= -0.7f)
+                        {
+                            m_FlagPos[i].x = 0.69f;
+                        }
+                        if (m_FlagPos[i].x <= -0.21f)
+                        {
+                            TweenAlpha.Begin(m_Country[i], 0.05f, 0.0f);
+                        }
+                        else if (m_FlagPos[i].x <= 0.54f)
+                        {
+                            TweenAlpha.Begin(m_Country[i], 0.1f, 1.0f);
+                        }
+                        break;
+                    case 3:
+                        m_FlagPos[i].x -= 0.01f;
+
+                       
+                        break;
                 }
             }
             if (m_team1.m_Right_RotateFlag == true)
             {
-                m_FlagPos[i].x += 0.01f;
-
-                if (m_FlagPos[i].x >= 0.49f)
+                switch (m_Flag[i])
                 {
-                    m_FlagPos[i].x = -0.3f;
+                    case 0:
+                        m_FlagPos[i].x += 0.01f;
+                        break;
+                    case 1:
+                        m_FlagPos[i].x += 0.042f;
+                        if (m_FlagPos[i].x >= 0.69f)
+                        {
+                            m_FlagPos[i].x = -0.7f;
+                        }
+                        break;
+                    case 2:
+                        m_FlagPos[i].x += 0.01f;
+                        break;
+                    case 3:
+                        m_FlagPos[i].x += 0.01f;
+
+                        break;
                 }
             }
             if (m_team1.m_Right_RotateFlag == false && m_team1.m_Left_RotateFlag == false)
