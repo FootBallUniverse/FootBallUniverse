57409e99c250768ad28fa7b1bd3a2f4b67665841
Takaya Takeuchi <epianyulu2013@gmail.com>
2015-01-05 03:04:01 +0900

    チュートリアルがスキップされるように（作業中のため）

--- Unity/FootBallUniverse/Assets/Tutorial/Scripts/TutorialManagerScript.cs
+++ Unity/FootBallUniverse/Assets/Tutorial/Scripts/TutorialManagerScript.cs
@@ -4,31 +4,129 @@ using System.Collections;
 public class TutorialManagerScript : MonoBehaviour {
 	GameObject[] guidVewer = new GameObject[3];
 
-	enum TUTORIAL_STATE
-	{
-		WAIT,
-		INIT,
-		FAID_IN,
-		TUTORIAL0,
-		TUTORIAL1,
-		TUTORIAL2,
-		TUTORIAL3,
-		FAID_OUT,
-		STATE_MAX
+	// チュートリアル用メッセージ（仮置）
+	string[] MainMessage = new string[24]{
+		// シーン１
+		"やあ、私は君たちのチーム…",
+		"代表の監督を努めさせてもらっている",
+		"だ。確か君たちは、このスタジアムは初めてだったね？",
+		"ここは、宇宙ステーション内に建設された球体スタジアムの中。これから“スペースフットボール”の、４年に１度の祭典…スペースワールドカップが行われる",
+		"地上のスタジアムのそれとは、比べ物にならない浮遊感だろう？試合が始まるまで少し時間がある。今のうちに、体を馴らしておこうか",
+		"まずは、そうだな… 上を見てくれ",
+		"今向かい合っている相手が、君のチームメイトだ。隣にいる、と言ったほうがわかりやすいかもしれないが",
+		"試合では、２人で協力して戦ってもらうことになる。今のうちに、連携を深めておいてくれ",
+		// シーン２
+		"次に、ボールの場所を確認しよう。[1P:左/2P:右]を見てくれ",
+		"見ての通り、ボールだ。試合では、そいつを相手チームのゴールに蹴りこむことを目指す",
+		"ボールは近づくと勝手に”ホールド”されて、足元についてくる",
+		"試しに近くまで移動して、ボールをホールドしてみるんだ",
+		"よし、いいぞ。次はボールを蹴ってみようか",
+		// シーン３
+		"チームメイトに向けて、”パス”を出してみよう。ボールは君が見ている場所に、まっすぐに飛ぶ",
+		"いいパスだ。体も動かしながら、何度かボールを蹴り合ってみようか",
+		"ボールを蹴る感覚も、だいぶ掴めてきたか？相手チームのキーパーに来てもらった。彼をかいくぐってゴールにシュートしてみよう",
+		"やるね！今の感覚は、しっかり覚えておくんだ",
+		"次は、相手チームの選手と一緒にウォーミングアップしようと思ったけど…まだ時間がかかるようだ。少し自由にしておいてくれ",
+		"次は、相手チームの選手と一緒にウォーミングアップしてみよう。本番と同じ環境で、ボールの奪い合いをしてみよう",
+		"相手チームの準備ができたみたいだ。本番と同じ環境で、ボールの奪い合いをしてみよう",
+		// シーン４
+		"正面に見えるのが、相手チームの選手たちだ。まずは、中央のボールを取って主導権を掴もう",
+		"よし、ボールを取ったな。相手はダッシュやタックルを使ってボールをスティールしようとしてくるはずだ。彼らをかわして、ゴールに突っ込め！",
+		"取られてしまったな。なら取り返すまでだ！ダッシュやタックルを使って追いつき、ボールをスティールしてやれ！",
+		"おっと、いつの間にかこんな時間だ。もうすぐ試合が始まってしまうな…君たちなら、きっと勝てる。最高の試合を見せてくれ！"
+		};
+	// チュートリアル用サブメッセージ（仮置）
+	string[] SubMessage = new string[6]{
+		"日本",
+		"イングランド",
+		"スペイン",
+		"ブラジル",
+		"左",
+		"右"
 	};
 
-	TUTORIAL_STATE state     = TUTORIAL_STATE.WAIT;
+	// チュートリアル状態遷移
+	enum TUTORIAL_STATE
+	{
+		// スタート処理
+		WAIT = 0,
+		INIT,
+		FAID_IN,
+		// 各自チュートリアル
+		SCENE0_Message00,
+		SCENE0_Message01,
+		SCENE0_Message02,
+		SCENE0_Message03,
+		SCENE0_Instruction00,
+		SCENE0_Play00,
+		SCENE0_Play01,
+		SCENE0_Message04,
+		SCENE0_Message05,
+		// 各自チュートリアル
+		SCENE1_Message00,
+		SCENE1_Instruction00,
+		SCENE1_Play00,
+		SCENE1_Message01,
+		SCENE1_Instruction01,
+		SCENE1_Message02,
+		SCENE1_Message03,
+		SCENE1_Instruction02,
+		SCENE1_Play01,
+		SCENE1_Message04,
+		// 各自チュートリアル
+		SCENE2_Message00,
+		SCENE2_Instruction00,
+		SCENE2_Play00,
+		SCENE2_Message01,
+		SCENE2_Play01,
+		SCENE2_Message02,
+		SCENE2_Play02,
+		SCENE2_Message03,
+		SCENE2_Message04,
+		SCENE2_Message05,
+		SCENE2_Message06,
+		SCENE2_Wait00,
+		// 合同チュートリアル（未完成）
+		SCENE3_Message00,
+		SCENE3_Play00,
+		SCENE3_Message01,
+		SCENE3_Message02,
+		SCENE3_Instruction00,
+		SCENE3_Message11,
+		SCENE3_Message12,
+		SCENE3_Message13,
+		SCENE3_Message04,
+		// 終了処理
+		FAID_OUT,
+		TUTORIAL_STATE_MAX
+	};
+
+	TUTORIAL_STATE[] state   = new TUTORIAL_STATE[2];
 	bool[] buttonCheck       = new bool[4];
+
 	GameObject[] buttonVewer = new GameObject[4];
 	GameObject[] bloackOut   = new GameObject[3];
+	GameObject[] messageLog  = new GameObject[2];
 
     CSoundPlayer m_soundPlayer;
 
 	// Use this for initialization
 	void Start () {
+#if true
+		Application.LoadLevel("MainGame");
+		return;
+#endif
 		// メインゲーム呼び出し
 		Application.LoadLevelAdditive("MainGame");
+		// 状態遷移設定
+		this.state[0] = this.state[1] = TUTORIAL_STATE.WAIT;
+		ReSetAllButtonCheck();
+		// オブジェクト挿入
+		for (int i = 0; i < 3; i++)
+			this.guidVewer[i] = GameObject.Find("GuidVewer" + i);
 
+		for (int i = 0; i < 4; i++)
+			this.buttonVewer[i] = GameObject.Find("Next_" + i);
 	}
 
 	//----------------------------------------------------------------------
@@ -61,6 +159,10 @@ public class TutorialManagerScript : MonoBehaviour {
 		this.bloackOut[0] = GameObject.Find("MainPanelFeed").gameObject;
 		this.bloackOut[1] = GameObject.Find("SubPanelFeed0").gameObject;
 		this.bloackOut[2] = GameObject.Find("SubPanelFeed1").gameObject;
+		// メッセージログ
+		this.messageLog[0] = GameObject.Find("SubPanel0").gameObject.transform.FindChild("MessageLog").gameObject;
+		this.messageLog[0] = GameObject.Find("SubPanel1").gameObject.transform.FindChild("MessageLog").gameObject;
+		
 		// 制限時間OFF
 		GameObject.Find("P1&P2").transform.FindChild("UI").transform.FindChild("Camera").transform.FindChild("Anchor").transform.FindChild("Panel").transform.FindChild("time_colon").gameObject.SetActive(false);
 		GameObject.Find("P1&P2").transform.FindChild("UI").transform.FindChild("Camera").transform.FindChild("Anchor").transform.FindChild("Panel").transform.FindChild("time_sec").gameObject.SetActive(false);
@@ -81,20 +183,38 @@ public class TutorialManagerScript : MonoBehaviour {
         m_soundPlayer = new CSoundPlayer();
         m_soundPlayer.PlayBGMFadeIn("tutorial/bgm_01", 0.05f);
 
-        // オブジェクト挿入
-		for (int i = 0; i < 3; i++)
-			this.guidVewer[i] = GameObject.Find("GuidVewer" + i);
-
-		for (int i = 0; i < 4; i++)
-			this.buttonVewer[i] = GameObject.Find("Next_" + i);
-
 		// 最初の画面に合わせて設定
 		this.guidVewer[0].SetActive(true);
 		this.guidVewer[0].GetComponent<UISprite>().spriteName = "user'sGuide_1";
 		this.guidVewer[1].SetActive(false);
 		this.guidVewer[2].SetActive(false);
 
-		ReSetButtonCheck();
+		ReSetAllButtonCheck();
+	}
+
+	//----------------------------------------------------------------------
+	// チーム２人のボタンを押したかを返す
+	//----------------------------------------------------------------------
+	// @Param   teamNo チーム番号（０で左、１で右）
+	// @Param   check  true:ボタンがおされた/false:ボタンがおされていない
+	// @Return  none
+	// @Date    2014/12/30  @Update 2014/12/30  @Author T.Takeuichi
+	//----------------------------------------------------------------------
+	bool GetButtonCheck(int teamNo)
+	{
+		if (teamNo == 0)
+		{
+			if (!this.buttonCheck[0]) return false;
+			if (!this.buttonCheck[1]) return false;
+		}
+		else if (teamNo == 1)
+		{
+			if (!this.buttonCheck[2]) return false;
+			if (!this.buttonCheck[3]) return false;
+		}
+		else return false;
+
+		return true;
 	}
 
 	//----------------------------------------------------------------------
@@ -105,7 +225,7 @@ public class TutorialManagerScript : MonoBehaviour {
 	// @Return  none
 	// @Date    2014/12/13  @Update 2014/12/13  @Author T.Takeuichi
 	//----------------------------------------------------------------------
-	bool GetButtonCheck()
+	bool GetAllButtonCheck()
 	{
 		for (int i = 0; i < 4; i++)
 			if(!this.buttonCheck[i])
@@ -115,13 +235,33 @@ public class TutorialManagerScript : MonoBehaviour {
 	}
 
 	//----------------------------------------------------------------------
-	// ボタンが押されたかのチェックを初期化
+	// チーム２人のボタンが押されたかのチェックを初期化
+	//----------------------------------------------------------------------
+	// @Param   teamNo チーム番号（０で左、１で右）
+	// @Return  none
+	// @Date    2014/12/30  @Update 2014/12/30  @Author T.Takeuichi
+	//----------------------------------------------------------------------
+	void ReSetButtonCheck(int teamNo)
+	{
+		if (teamNo == 0)
+		{
+			this.buttonCheck[0] = false;
+			this.buttonCheck[1] = false;
+		}else if (teamNo == 1){
+			this.buttonCheck[2] = false;
+			this.buttonCheck[3] = false;
+		}
+		else return;
+	}
+
+	//----------------------------------------------------------------------
+	// ４人のボタンが押されたかのチェックを初期化
 	//----------------------------------------------------------------------
 	// @Param   none
 	// @Return  none
 	// @Date    2014/12/13  @Update 2014/12/13  @Author T.Takeuichi
 	//----------------------------------------------------------------------
-	void ReSetButtonCheck()
+	void ReSetAllButtonCheck()
 	{
 		for (int i = 0; i < 4; i++)
 			this.buttonCheck[i] = false;
@@ -143,73 +283,149 @@ public class TutorialManagerScript : MonoBehaviour {
 		}
 	}
 
+	//----------------------------------------------------------------------
+	// ボタンが押されたらチェックを入れる（Ａボタン）
+	//----------------------------------------------------------------------
+	// @Param   none
+	// @Return  none
+	// @Date    2014/12/30  @Update 2014/12/30  @Author T.Takeuichi
+	//----------------------------------------------------------------------
+	void SetButton_A()
+	{
+		if (Input.GetKeyDown(InputXBOX360.P1_XBOX_A) && this.buttonCheck[0] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[0] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P2_XBOX_A) && this.buttonCheck[1] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[1] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P3_XBOX_A) && this.buttonCheck[2] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[2] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P4_XBOX_A) && this.buttonCheck[3] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[3] = true;
+		}
+		// DEBUG用（ＰＣ）
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[0] = this.buttonCheck[1] = true;
+		}
+		if (Input.GetKeyDown(KeyCode.RightShift))
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[2] = this.buttonCheck[3] = true;
+		}
+	}
+
+	//----------------------------------------------------------------------
+	// ボタンが押されたらチェックを入れる（STARTボタン）
+	//----------------------------------------------------------------------
+	// @Param   none
+	// @Return  none
+	// @Date    2014/12/30  @Update 2014/12/30  @Author T.Takeuichi
+	//----------------------------------------------------------------------
+	void SetButton_START()
+	{
+		if (Input.GetKeyDown(InputXBOX360.P1_XBOX_START) && this.buttonCheck[0] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[0] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P2_XBOX_START) && this.buttonCheck[1] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[1] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P3_XBOX_START) && this.buttonCheck[2] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[2] = true;
+		}
+		if (Input.GetKeyDown(InputXBOX360.P4_XBOX_START) && this.buttonCheck[3] == false)
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[3] = true;
+		}
+		// DEBUG用（ＰＣ）
+		if (Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[0] = this.buttonCheck[1] = true;
+		}
+		if (Input.GetKeyDown(KeyCode.RightShift))
+		{
+			m_soundPlayer.PlaySE("tutorial/button_push");
+			this.buttonCheck[2] = this.buttonCheck[3] = true;
+		}
+	}
+
+
+
 	// Update is called once per frame
 	void Update()
 	{
+		Debug.Log(this.state[0] + " /" + this.state[1]);
+		for (int i = 0; i < 2; i++)
+		{
+			switch (this.state[i])
+			{
+				case TUTORIAL_STATE.WAIT: this.state[i] = TUTORIAL_STATE.INIT; break;
+				case TUTORIAL_STATE.INIT:
+					if (i == 0) InitTutorial();
+					this.state[i] = TUTORIAL_STATE.FAID_IN;
+					break;
+
+				case TUTORIAL_STATE.FAID_IN:
+					if ( !this.bloackOut[0].GetComponent<TweenAlpha>().enabled &&
+				         !this.bloackOut[1].GetComponent<TweenAlpha>().enabled &&
+				         !this.bloackOut[2].GetComponent<TweenAlpha>().enabled )
+						this.state[i] = TUTORIAL_STATE.SCENE0_Message00;break;
+
+				case TUTORIAL_STATE.SCENE0_Message00:
+				case TUTORIAL_STATE.SCENE0_Message01:
+				case TUTORIAL_STATE.SCENE0_Message02:
+				case TUTORIAL_STATE.SCENE0_Message03:
+				case TUTORIAL_STATE.SCENE0_Message04:
+					SetButton_A();
+
+					// 次へ遷移
+					if (GetButtonCheck(i))
+					{
+						this.messageLog[i].GetComponent<UILabel>().text = this.MainMessage[0];
+						ReSetButtonCheck(i);
+						this.state[i] += 1;
+					}
+					break;
+
+				case TUTORIAL_STATE.SCENE0_Instruction00:
+					break;
+
+				case TUTORIAL_STATE.SCENE0_Play00:
+					break;
+			}
+		}
 		// Input
-        if (Input.GetKeyDown(InputXBOX360.P1_XBOX_START) && this.buttonCheck[0] == false)
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[0] = true;
-        }
-        if (Input.GetKeyDown(InputXBOX360.P2_XBOX_START) && this.buttonCheck[1] == false)
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[1] = true;
-        }
-        if (Input.GetKeyDown(InputXBOX360.P3_XBOX_START) && this.buttonCheck[2] == false)
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[2] = true;
-        }
-        if (Input.GetKeyDown(InputXBOX360.P4_XBOX_START) && this.buttonCheck[3] == false)
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[3] = true;
-        }
-        if (Input.GetKeyDown(KeyCode.LeftShift))
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[0] = this.buttonCheck[1] = true;
-        }
-        if (Input.GetKeyDown(KeyCode.RightShift))
-        {
-            m_soundPlayer.PlaySE("tutorial/button_push");
-            this.buttonCheck[2] = this.buttonCheck[3] = true;
-        }
+		//SetButton_START();
 
 		// 遷移
-		switch (this.state)
+		/*
+		switch (this.state[0])
 		{
-			case TUTORIAL_STATE.WAIT:
-				this.state = TUTORIAL_STATE.INIT;
-				return;
-
-
-			case TUTORIAL_STATE.INIT:
-				InitTutorial();
-				this.state = TUTORIAL_STATE.FAID_IN;
-				break;
-
-
-			case TUTORIAL_STATE.FAID_IN:
-				// 仮
-
-				if ( !this.bloackOut[0].GetComponent<TweenAlpha>().enabled &&
-				     !this.bloackOut[1].GetComponent<TweenAlpha>().enabled &&
-				     !this.bloackOut[2].GetComponent<TweenAlpha>().enabled )
-				{
-					ReSetButtonCheck();
-					this.state = TUTORIAL_STATE.TUTORIAL0;
-				}
-				break;
 
 
 			case TUTORIAL_STATE.TUTORIAL0:
-				if (GetButtonCheck())
+				if (GetAllButtonCheck())
 				{
                     m_soundPlayer.PlaySE("tutorial/tutorial_next");
-					ReSetButtonCheck();
+					ReSetAllButtonCheck();
 					this.state = TUTORIAL_STATE.TUTORIAL1;
 				}
 				break;
@@ -222,10 +438,10 @@ public class TutorialManagerScript : MonoBehaviour {
 				this.guidVewer[2].SetActive(true);
 				this.guidVewer[2].GetComponent<UISprite>().spriteName = "user'sGuide_3";
 
-				if (GetButtonCheck())
+				if (GetAllButtonCheck())
 				{
                     m_soundPlayer.PlaySE("tutorial/tutorial_next");
-					ReSetButtonCheck();
+					ReSetAllButtonCheck();
 					this.state = TUTORIAL_STATE.TUTORIAL2;
 				}
 				break;
@@ -236,10 +452,10 @@ public class TutorialManagerScript : MonoBehaviour {
 				this.guidVewer[2].SetActive(true);
 				this.guidVewer[2].GetComponent<UISprite>().spriteName = "user'sGuide_5";
 
-				if (GetButtonCheck())
+				if (GetAllButtonCheck())
 				{
                     m_soundPlayer.PlaySE("tutorial/tutorial_next");
-					ReSetButtonCheck();
+					ReSetAllButtonCheck();
 					this.state = TUTORIAL_STATE.TUTORIAL3;
 				}
 				break;
@@ -250,7 +466,7 @@ public class TutorialManagerScript : MonoBehaviour {
 				this.guidVewer[0].GetComponent<UISprite>().spriteName = "user'sGuide_6";
 				this.guidVewer[1].SetActive(false);
 				this.guidVewer[2].SetActive(false);
-				if (GetButtonCheck())
+				if (GetAllButtonCheck())
 				{
                     m_soundPlayer.PlaySE("tutorial/tutorial_next");
 					for (int i = 0; i < 3; i++)
@@ -272,7 +488,7 @@ public class TutorialManagerScript : MonoBehaviour {
 				}
 				break;
 		}
-
+		*/
 		ButtonDraw();
 	}
 }
